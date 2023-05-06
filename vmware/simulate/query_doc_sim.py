cbe64f103fc8fbcbc31b4b11ea43014ede9bb18a
Doug Turnbull <softwaredoug@gmail.com>
2023-05-06 07:19:00 -0400

    Fix the exception handling

--- vmware/simulate/query_doc_sim.py
+++ vmware/simulate/query_doc_sim.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from time import perf_counter
 from elasticsearch import Elasticsearch
+from elasticsearch.exceptions import ConnectionError, NotFoundError
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import matplotlib.pyplot as plt
 
@@ -16,40 +17,49 @@ from vmware.search.passage_similarity import passage_similarity_long_lines, cach
 
 def passage_similarity(es, query, doc_id):
     """Get passage similarity scores."""
-    result = es.get(index="vmware", id=doc_id)
-    passage_similarity_long_lines(query, result)
-    similarities = {}
-    for field in cached_fields():
-        similarities[field] = float(result['_source'][field])
-    return similarities
+    tries = 0
+    while tries < 3:
+        try:
+            result = es.get(index="vmware", id=doc_id)
+            passage_similarity_long_lines(query, result)
+            similarities = {}
+            for field in cached_fields():
+                similarities[field] = float(result['_source'][field])
+            return similarities
+        except (ConnectionError, NotFoundError):
+            print("*******************************************")
+            print("Elasticsearch Connection error, retrying...")
+            tries += 1
+            continue
 
 
 def sim_worker(es, result):
-    es = Elasticsearch()
     query = result['Query']
     doc_id = result['DocumentId']
     start = perf_counter()
     sims = passage_similarity(es, query, doc_id)
     print(query, doc_id, perf_counter() - start)
-    return {**result, **sims}
+    if sims is None:
+        return result
+    else:
+        return {**result, **sims}
 
 
 def score_similarity(simulated_results: pd.DataFrame, num_to_score: int = None) -> pd.DataFrame:
     """Score similarity."""
     new_df = []
-    es = Elasticsearch()
+    es = Elasticsearch(read_timout=300, timeout=300)
     tasks = []
-    batch_size = min(100, num_to_score) if num_to_score is not None else 100
-    with ThreadPoolExecutor(max_workers=1) as executor:
+    batch_size = 1000
+    with ThreadPoolExecutor(max_workers=10) as executor:
         for idx, result in enumerate(simulated_results.sample(frac=1).to_dict(orient='records')):
             tasks.append(executor.submit(sim_worker, es, result))
 
             if len(tasks) >= batch_size:
-                print(f"Waiting for completion... {idx}, {num_to_score}")
                 for task in as_completed(tasks):
                     new_df.append(task.result())
                 tasks = []
-                if num_to_score is not None and len(new_df) >= num_to_score:
+                if num_to_score is not None and idx >= num_to_score:
                     break
     return pd.DataFrame(new_df)
 
@@ -69,25 +79,33 @@ def build_hist(scored_results, field):
     for idx, bucket in enumerate(assigned_buckets):
         hist1[bucket] += scored_results['grade'].iloc[idx]
         hist0[bucket] += (1 - scored_results['grade'].iloc[idx])
-    return hist1, hist0
+    return hist0, hist1
 
 
 def plot(hist0, hist1):
     x = np.arange(0, 1.00, 0.01)
-    plt.axis([0, 1, 0, 1 + max(max(hist0), max(hist1))])
-    weighted_sum0 = np.sum(np.array(hist0) * np.array(np.arange(0, 1.00, 0.01)))
-    mean0 = weighted_sum0 / np.sum(hist0)
-    weighted_sum1 = np.sum(np.array(hist1) * np.array(np.arange(0, 1.00, 0.01)))
-    mean1 = weighted_sum1 / np.sum(hist1)
-    y0 = hist0
-    y1 = hist1
-    # Plot 0s as red
-    plt.bar(x, y0, color='r', width=0.01)
-    # Plot 1s as green
-    plt.bar(x, y1, color='g', width=0.01)
+    if hist0 is not None and hist1 is not None:
+        plt.axis([0, 1, 0, 1 + max(max(hist0), max(hist1))])
+    elif hist0 is not None:
+        plt.axis([0, 1, 0, 1 + max(hist0)])
+    elif hist1 is not None:
+        plt.axis([0, 1, 0, 1 + max(hist1)])
 
-    plt.axvline(x=mean0, color='r', linestyle='dashed', linewidth=1)
-    plt.axvline(x=mean1, color='g', linestyle='dashed', linewidth=1)
+    if hist0 is not None:
+        weighted_sum0 = np.sum(np.array(hist0) * np.array(np.arange(0, 1.00, 0.01)))
+        mean0 = weighted_sum0 / np.sum(hist0)
+        y0 = hist0
+        # Plot 0s as red
+        plt.bar(x, y0, color='r', width=0.01)
+        plt.axvline(x=mean0, color='r', linestyle='dashed', linewidth=1)
+    if hist1 is not None:
+        weighted_sum1 = np.sum(np.array(hist1) * np.array(np.arange(0, 1.00, 0.01)))
+        mean1 = weighted_sum1 / np.sum(hist1)
+        y1 = hist1
+        # Plot 1s as green
+        plt.bar(x, y1, color='g', width=0.01)
+        plt.axvline(x=mean1, color='g', linestyle='dashed', linewidth=1)
+    plt.xlabel('feature value', fontsize=20)
     plt.legend()
     plt.show()
 
